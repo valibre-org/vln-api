2e2859a714cd5444f0bf4d7d0fa551a047de032a
Stanly Johnson <stanlyjohnson@outlook.com>
2021-02-21 17:20:17 +0530

    convert to trait

--- plugins/blockchain/src/util_txid.rs
+++ plugins/blockchain/src/util_txid.rs
@@ -1,15 +1,34 @@
-/// Gives a short friendly transaction-id from a transaction hash
-/// by encoding the first 32bits to base58
-/// ```
-/// use blockchain::util_txid::get_short_tx_id;
-/// let result = get_short_tx_id("0xd7bd44af293e45e0dc7583d12c6e75492410b3d74b01fe87371dc2eea1637a57");
-/// assert_eq!("6WquqG".as_bytes().to_vec(), result);
-/// ```
-pub fn get_short_tx_id(input: &str) -> Vec<u8> {
-    let input = if input.starts_with("0x") {
-        hex::decode(&input[2..]).unwrap_or_else(|_| input.into())
-    } else {
-        input.into()
-    };
-    bs58::encode(&input[0..4]).into_vec()
+/// Trait that allows formatting input to a short/prettier format
+pub trait PrettyFormat {
+    /// Denotes the min length required for formatting
+    const MIN_LENGTH: usize;
+    /// Method to convert input to prettier format
+    fn pretty(&self) -> Result<Vec<u8>, &'static str>;
+}
+
+impl<T> PrettyFormat for T
+where
+    T: AsRef<[u8]>,
+{
+    const MIN_LENGTH: usize = 4;
+
+    /// Gives a short friendly transaction-id from a transaction hash
+    /// by encoding the first 32bits to base58
+    fn pretty(&self) -> Result<Vec<u8>, &'static str> {
+        let _x = self.as_ref();
+        if _x.len() >= Self::MIN_LENGTH {
+            Ok(bs58::encode(&_x[0..Self::MIN_LENGTH]).into_vec())
+        } else {
+            Err("Input too small!")
+        }
+    }
+}
+
+#[test]
+fn test_pretty() {
+    assert_eq!(
+        "d7bd44af293e45e0dc7583d12".pretty(),
+        Ok("3ZaN9y".as_bytes().to_vec())
+    );
+    assert_eq!("foo".pretty(), Err("Input too small!"));
 }
