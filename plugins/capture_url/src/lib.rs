28166e50ff495d4bb6281ca07582f6276e09e5b1
Daniel Olano <daniel@olanod.com>
2021-04-18 21:48:42 +0200

    Update capture_url plugin to valor 0.5

--- plugins/capture_url/src/lib.rs
+++ plugins/capture_url/src/lib.rs
@@ -1,11 +1,34 @@
-use http::{content::Accept, mime, Request, Response};
-use lazy_static::lazy_static;
+use http::{content::Accept, mime, Method, Request, Response, StatusCode};
 use std::collections::HashMap;
-use std::env;
 use std::num::ParseIntError;
 use std::str::FromStr;
 use thirtyfour::{prelude::*, OptionRect};
+use url::Url;
 use valor::*;
+
+const LOCAL_WEB_DRIVER_HOST: &str = "http://localhost:4444/wd/hub";
+
+#[vlugin]
+pub async fn on_create(cx: &mut Context) {
+    cx.set(
+        cx.config::<String>()
+            .map(|host| host.parse::<Url>().ok())
+            .flatten()
+            .unwrap_or_else(|| LOCAL_WEB_DRIVER_HOST.parse().unwrap()),
+    );
+}
+
+pub async fn on_request(cx: &Context, req: Request) -> Response {
+    let host = cx.get::<Url>();
+    let url = req.url();
+    let method = req.method();
+
+    match (url.path(), method) {
+        ("/", Method::Get) => capture_handler(&host, req).await,
+        ("/", _) => StatusCode::MethodNotAllowed.into(),
+        _ => StatusCode::NotFound.into(),
+    }
+}
 
 struct ImageDimensions {
     width: u16,
@@ -28,27 +51,18 @@ impl FromStr for ImageDimensions {
     }
 }
 
-lazy_static! {
-    static ref WEB_DRIVER_HOST: String = {
-        let host = match env::var("CAPTURE_URL_WEB_DRIVER_HOST") {
-            Ok(host) => host,
-            _ => "http://localhost:4444/wd/hub".to_owned(),
-        };
-
-        let _ = Url::parse(&host)
-            .expect("Env var `CAPTURE_URL_WEB_DRIVER_HOST` expected to be a valid URL");
-        host
-    };
-}
-
-async fn capture_url(url: &Url, dimensions: &Option<ImageDimensions>) -> WebDriverResult<Vec<u8>> {
+async fn capture_url(
+    host: &Url,
+    url: &Url,
+    dimensions: &Option<ImageDimensions>,
+) -> WebDriverResult<Vec<u8>> {
     let caps = {
         let mut caps = DesiredCapabilities::firefox();
         caps.set_headless()?;
         caps
     };
 
-    let driver = WebDriver::new(&WEB_DRIVER_HOST, &caps).await?;
+    let driver = WebDriver::new(host.as_str(), &caps).await?;
     let ImageDimensions { width, height } =
         dimensions.as_ref().unwrap_or_else(|| &ImageDimensions {
             width: 450,
@@ -71,7 +85,7 @@ async fn capture_url(url: &Url, dimensions: &Option<ImageDimensions>) -> WebDriv
     driver.screenshot_as_png().await
 }
 
-async fn capture_handler(req: Request) -> Response {
+async fn capture_handler(host: &Url, req: Request) -> Response {
     let hash_query: HashMap<_, _> = req.url().query_pairs().into_owned().collect();
     let url = hash_query.get("url");
     let dimensions = hash_query
@@ -95,7 +109,7 @@ async fn capture_handler(req: Request) -> Response {
         _ => return StatusCode::BadRequest.into(),
     };
 
-    let image = match capture_url(&url, &dimensions).await {
+    let image = match capture_url(host, &url, &dimensions).await {
         Ok(image) => image,
         _ => return StatusCode::InternalServerError.into(),
     };
@@ -112,18 +126,6 @@ async fn capture_handler(req: Request) -> Response {
 
     res.set_content_type(response_mime.clone());
     res
-}
-
-#[vlugin]
-async fn handler(req: Request) -> Response {
-    let url = req.url();
-    let method = req.method();
-
-    match (url.path(), method) {
-        ("/", Method::Get) => capture_handler(req).await,
-        ("/", _) => StatusCode::MethodNotAllowed.into(),
-        _ => StatusCode::NotFound.into(),
-    }
 }
 
 #[cfg(test)]
@@ -155,7 +157,7 @@ mod tests {
             request
         };
 
-        let mut response = handler(request).await;
+        let mut response = on_request(&test_context().await, request).await;
         let buffer = response.body_bytes().await.unwrap();
         let image = image::load_from_memory_with_format(&buffer, image::ImageFormat::Png).unwrap();
         let actual_dimensions = image.dimensions();
@@ -164,7 +166,7 @@ mod tests {
             (u32::from(width), u32::from(height))
         };
 
-        assert!(actual_dimensions == expected_dimensions);
+        assert_eq!(actual_dimensions, expected_dimensions);
     }
 
     #[async_std::test]
@@ -192,13 +194,13 @@ mod tests {
             request
         };
 
-        let mut response = handler(request).await;
+        let mut response = on_request(&test_context().await, request).await;
         let base64_image = response.body_string().await.unwrap();
         let splitted_data_uri: Vec<&str> = base64_image.split(',').collect();
 
         // check if data uri schema is the one expected
         let data_schema = *splitted_data_uri.get(0).unwrap();
-        assert!(data_schema == "data:image/png;base64");
+        assert_eq!(data_schema, "data:image/png;base64");
 
         // check if base64 data is a valid png
         let image_data = *splitted_data_uri.get(1).unwrap();
@@ -210,6 +212,12 @@ mod tests {
             (u32::from(width), u32::from(height))
         };
 
-        assert!(actual_dimensions == expected_dimensions);
+        assert_eq!(actual_dimensions, expected_dimensions);
+    }
+
+    async fn test_context() -> Context {
+        let mut cx = Context::default();
+        on_create(&mut cx).await;
+        cx
     }
 }
