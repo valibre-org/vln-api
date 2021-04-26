a2e84ba6d0ba5c7b6e983eae5b14b370fec92a55
Jorge Villalobos <jorge.villalobos2809@gmail.com>
2021-04-26 13:44:53 -0500

    Update to latest valor

--- plugins/html_renderer/src/lib.rs
+++ plugins/html_renderer/src/lib.rs
@@ -1,11 +1,14 @@
 mod template_renderer;
 mod utils;
 
-use http::convert::{json, Deserialize, Serialize};
-use lazy_static::lazy_static;
+use http::{
+    convert::{json, Deserialize, Serialize},
+    Body, Method, Request, Response, StatusCode,
+};
 use path_tree::PathTree;
 use serde_json::Value as JsonValue;
 use template_renderer::TemplateRenderer;
+use url::Url;
 use utils::format_html_data_url;
 use valor::*;
 
@@ -42,100 +45,93 @@ enum Cmd {
     Unknown,
 }
 
-lazy_static! {
-    static ref ROUTER: PathTree<Cmd> = {
+#[vlugin]
+pub async fn on_create(cx: &mut Context) {
+    cx.set(TemplateRenderer::default());
+}
+
+pub async fn on_request(cx: &Context, mut req: Request) -> Response {
+    let router = {
         let mut p = PathTree::new();
         p.insert("/templates", Cmd::Templates);
         p.insert("/render", Cmd::RenderTemplate);
         p.insert("*", Cmd::Unknown);
         p
     };
-}
+    let html_renderer = cx.get::<TemplateRenderer>();
+    let (action, _params) = {
+        router
+            .find(req.url().path())
+            .unwrap_or_else(|| (&Cmd::Unknown, vec![]))
+    };
 
-#[vlugin]
-#[derive(Default)]
-struct Handler<'a> {
-    html_renderer: TemplateRenderer<'a>,
-}
+    match (action, req.method()) {
+        (Cmd::Templates, Method::Get) => json!(html_renderer.get_templates()).into(),
+        (Cmd::Templates, Method::Post) => {
+            let template = match req.body_json().await {
+                Ok(RegisterTemplateInput { template }) => template,
+                _ => return StatusCode::BadRequest.into(),
+            };
 
-#[async_trait(?Send)]
-impl RequestHandler for Handler<'_> {
-    async fn handle_request(&self, mut req: Request) -> Response {
-        let (action, _params) = {
-            ROUTER
-                .find(req.url().path())
-                .unwrap_or_else(|| (&Cmd::Unknown, vec![]))
-        };
+            let template_id = match html_renderer.register_template(&template) {
+                Ok(template_id) => template_id,
+                Err(_) => return StatusCode::BadRequest.into(),
+            };
 
-        match (action, req.method()) {
-            (Cmd::Templates, Method::Get) => json!(self.html_renderer.get_templates()).into(),
-            (Cmd::Templates, Method::Post) => {
-                let template = match req.body_json().await {
-                    Ok(RegisterTemplateInput { template }) => template,
-                    _ => return StatusCode::BadRequest.into(),
+            let response = {
+                let mut response = Response::new(StatusCode::Created);
+                let body = match Body::from_json(&RegisterTemplatePayload { template_id }) {
+                    Ok(body) => body,
+                    _ => return StatusCode::InternalServerError.into(),
                 };
-
-                let template_id = match self.html_renderer.register_template(&template) {
-                    Ok(template_id) => template_id,
-                    Err(_) => return StatusCode::BadRequest.into(),
-                };
-
-                let response = {
-                    let mut response = Response::new(StatusCode::Created);
-                    let body = match Body::from_json(&RegisterTemplatePayload { template_id }) {
-                        Ok(body) => body,
-                        _ => return StatusCode::InternalServerError.into(),
-                    };
-                    response.set_body(body);
-                    response
-                };
+                response.set_body(body);
                 response
-            }
-            (Cmd::RenderTemplate, Method::Post) => {
-                let (template_id, data, output) = match req.body_json().await {
-                    Ok(RenderTemplateInput {
-                        template_id,
-                        data,
-                        output,
-                    }) => (template_id, data, output),
-                    _ => return StatusCode::BadRequest.into(),
-                };
-
-                let rendered_template =
-                    match self.html_renderer.render_template(&template_id, &data) {
-                        Some(rendered_template) => rendered_template,
-                        _ => return StatusCode::BadRequest.into(),
-                    };
-
-                match output {
-                    Some(RenderOutput::DataUrl {
-                        redirect_url: Some(redirect_url),
-                        query_param_name,
-                    }) => {
-                        let data_url = format_html_data_url(&rendered_template);
-                        let query_param_name = query_param_name.unwrap_or("url".into());
-                        let url = {
-                            match Url::parse_with_params(
-                                &redirect_url,
-                                &[(query_param_name, &data_url)],
-                            ) {
-                                Ok(url) => url,
-                                _ => return StatusCode::BadRequest.into(),
-                            }
-                        };
-                        let mut response = Response::new(StatusCode::SeeOther);
-                        let _ = response.insert_header("Location", url.to_string());
-                        response
-                    }
-                    Some(RenderOutput::DataUrl { .. }) => {
-                        format_html_data_url(&rendered_template).into()
-                    }
-                    _ => rendered_template.into(),
-                }
-            }
-            (Cmd::Templates, _) | (Cmd::RenderTemplate, _) => StatusCode::MethodNotAllowed.into(),
-            (Cmd::Unknown, _) => StatusCode::NotFound.into(),
+            };
+            response
         }
+        (Cmd::RenderTemplate, Method::Post) => {
+            let (template_id, data, output) = match req.body_json().await {
+                Ok(RenderTemplateInput {
+                    template_id,
+                    data,
+                    output,
+                }) => (template_id, data, output),
+                _ => return StatusCode::BadRequest.into(),
+            };
+
+            let rendered_template = match html_renderer.render_template(&template_id, &data) {
+                Some(rendered_template) => rendered_template,
+                _ => return StatusCode::BadRequest.into(),
+            };
+
+            match output {
+                Some(RenderOutput::DataUrl {
+                    redirect_url: Some(redirect_url),
+                    query_param_name,
+                }) => {
+                    let data_url = format_html_data_url(&rendered_template);
+                    let query_param_name = query_param_name.unwrap_or("url".into());
+                    let url = {
+                        match Url::parse_with_params(
+                            &redirect_url,
+                            &[(query_param_name, &data_url)],
+                        ) {
+                            Ok(url) => url,
+                            _ => return StatusCode::BadRequest.into(),
+                        }
+                    };
+                    let mut response = Response::new(StatusCode::SeeOther);
+                    let _ = response.insert_header("Location", url.to_string());
+                    response
+                }
+                Some(RenderOutput::DataUrl { .. }) => {
+                    format_html_data_url(&rendered_template).into()
+                }
+                _ => rendered_template.into(),
+            }
+        }
+        (Cmd::Templates, _) | (Cmd::RenderTemplate, _) => StatusCode::MethodNotAllowed.into(),
+        (Cmd::Unknown, _) => StatusCode::NotFound.into(),
     }
 }
 
@@ -145,20 +141,19 @@ mod tests {
 
     #[async_std::test]
     async fn it_registers_template() {
-        let handler = super::Handler::default();
         let template = "<p>Hello, world.</p>";
-        let template_id = register_template(&handler, template.into()).await;
+        let template_id = register_template(&test_context().await, template.into()).await;
 
         assert!(template_id.len() > 0);
     }
 
     #[async_std::test]
     async fn it_stores_registered_template() {
-        let handler = super::Handler::default();
         let template = "<p>Hello, world.</p>";
-        let template_id = register_template(&handler, template.into()).await;
+        let cx = test_context().await;
+        let template_id = register_template(&cx, template.into()).await;
         let request = Request::new(Method::Get, "http://localhost/templates");
-        let mut response = handler.handle_request(request).await;
+        let mut response = on_request(&cx, request).await;
         let output: Vec<String> = response.body_json().await.unwrap();
 
         assert_eq!(output, vec![template_id.clone()]);
@@ -166,9 +161,11 @@ mod tests {
 
     #[async_std::test]
     async fn it_renders_registered_template_with_data() {
-        let handler = super::Handler::default();
         let template = "<p>Hello, {{firstname}} {{lastname}}.</p>";
-        let template_id = register_template(&handler, template.into()).await;
+        let cx = test_context().await;
+        let template_id = register_template(&cx, template.into()).await;
+
+        dbg!(&template_id);
 
         let request = {
             let input = RenderTemplateInput {
@@ -181,7 +178,7 @@ mod tests {
             request.set_body(request_body);
             request
         };
-        let mut response = handler.handle_request(request).await;
+        let mut response = on_request(&cx, request).await;
         let output = response.body_string().await.unwrap();
 
         assert_eq!(output, "<p>Hello, John Doe.</p>");
@@ -189,9 +186,9 @@ mod tests {
 
     #[async_std::test]
     async fn it_redirects_to_url_with_image_data() {
-        let handler = super::Handler::default();
         let template = "<p>Hello, {{firstname}} {{lastname}}.</p>";
-        let template_id = register_template(&handler, template.into()).await;
+        let cx = test_context().await;
+        let template_id = register_template(&cx, template.into()).await;
 
         let request = {
             let input = RenderTemplateInput {
@@ -208,7 +205,7 @@ mod tests {
             request
         };
 
-        let response = handler.handle_request(request).await;
+        let response = on_request(&cx, request).await;
         assert_eq!(response.status(), StatusCode::SeeOther);
 
         let location = response
@@ -221,7 +218,7 @@ mod tests {
         assert_eq!(location, expected_location);
     }
 
-    async fn register_template<'a>(handler: &super::Handler<'a>, template: &str) -> String {
+    async fn register_template<'a>(cx: &Context, template: &str) -> String {
         let request = {
             let input = RegisterTemplateInput {
                 template: template.into(),
@@ -232,8 +229,14 @@ mod tests {
             request
         };
 
-        let mut response = handler.handle_request(request).await;
+        let mut response = on_request(cx, request).await;
         let RegisterTemplatePayload { template_id } = response.body_json().await.unwrap();
         template_id
+    }
+
+    async fn test_context() -> Context {
+        let mut cx = Context::default();
+        on_create(&mut cx).await;
+        cx
     }
 }
